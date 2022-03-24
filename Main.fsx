f8702fa036662f567dc73badd5af0d058725bcb4
s202771 <s202771@student.dtu.dk>
2022-03-24 22:37:01 +0100

    Works for command Sequence

--- Main.fsx
+++ Main.fsx
@@ -29,36 +29,62 @@ let getMemString (mem:Mem) = // Only works for variables so far
     vars |> List.iter(fun (id, value) -> s <- s + id + "=" + string value + ", ")
     s
 
-let rec getAexprValue e vars =
+let getVariableValue var mem =
+    let mutable temp = 0
+    let (vars, arrs) = mem
+    vars |> List.iter(fun (id, value) -> if id = var then temp <- value
+                                                     )
+    temp
+
+let rec getAexprValue (e, mem:Mem) =
   match e with
     | Num(x) -> x
-    | Identifier(x) -> let mutable r = 0 // If i don't find x, we return 0, which should be
-                       List.iter(fun (id, value) -> if id = x then r <- value else failwith "unkown raviable") vars
-                       r
+    | Identifier(x) -> getVariableValue x mem
     | IdentifierArray(x, y) -> 1
-    | TimesExpr(x,y) -> getAexprValue x vars  * getAexprValue y vars
-    | DivExpr(x,y) -> getAexprValue x vars  / getAexprValue y vars
-    | PlusExpr(x,y) -> getAexprValue x vars  + getAexprValue y vars
-    | MinusExpr(x,y) -> getAexprValue x vars  - getAexprValue y vars
+    | TimesExpr(x,y) -> getAexprValue (x, mem)  * getAexprValue (y, mem)
+    | DivExpr(x,y) -> getAexprValue (x, mem)  / getAexprValue (y, mem)
+    | PlusExpr(x,y) -> getAexprValue (x, mem)  + getAexprValue (y, mem)
+    | MinusExpr(x,y) -> getAexprValue (x, mem)  - getAexprValue (y, mem)
     | PowExpr(x,y) -> 2
-    | UPlusExpr(x) -> getAexprValue x vars 
-    | UMinusExpr(x) -> - getAexprValue x vars 
+    | UPlusExpr(x) -> getAexprValue (x, mem) 
+    | UMinusExpr(x) -> - getAexprValue (x, mem) 
 
 let performCalc action mem =
     match action with
         | Assignment(var, expr) ->  let varText = getTextAri var
-                                    let result = getAexprValue expr mem 
-                                    mem |> List.mapi (fun i (id, value) -> if varText = id then (varText, result) else (id, value))
+                                    let result = getAexprValue (expr, mem) 
+                                    let (vars, ari) = mem
+                                    let newVars = vars |> List.mapi (fun i (id, value) -> if id = varText then (id, result) else (id, value))
+                                    Mem(newVars,ari)
                                     
         | SkipAction -> mem
 
 
+let filterEdgesByStart (pg:list<Edge>, node:int) = 
+    pg |> List.filter (fun (s,_,_) -> s = node)
 
 
+let applyActionToMemory s act e mem =
+    printfn "\nEdge: (q%d, %s, q%d)" s (printAction act) e
+    printfn "Before memory: %s" (getMemString mem)
+    let newMem = performCalc act mem
+    Console.ReadKey()
+    printf "After: memory: %s" (getMemString newMem)
+    Console.ReadLine()
+    newMem
 
-let stepWiseExecute (pg:list<Edge>, mem:Mem)=
-    let startEdges = pg |> List.filter (fun (s,_,_) -> s = 0)
-    printProgramGraph startEdges
+let rec visitEdges (edges:list<Edge>, pg:list<Edge>, mem:Mem) =
+    match edges with
+    | (s, act, e) :: tail ->let memNew = applyActionToMemory s act e mem 
+                            let nextEdges = filterEdgesByStart (pg, e)
+                            visitEdges (nextEdges, pg, memNew)
+                            //visitEdges tail mem pg 
+    | [] -> mem
+
+let startStepWiseExecute (pg:list<Edge>, mem:Mem)=
+    let startEdges = filterEdgesByStart (pg, 0)
+    let m = visitEdges (startEdges, pg, mem)
+    m
 
 
 let compileFromFile n =
@@ -72,7 +98,7 @@ let compileFromFile n =
     let mem = initializeMemory 0
     printfn "%s" (getMemString mem)
 
-    stepWiseExecute (pg, mem)
+    startStepWiseExecute (pg, mem)
 
     // let lastNode = (getDepth e) + 1
     // printfn "Last node will be: %d" lastNode
